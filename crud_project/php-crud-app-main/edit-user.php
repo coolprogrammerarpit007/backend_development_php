ebf329caf205a5db37f654375522ab0e9405c529
arpit mishra <arpit.mishra.out@gmail.com>
2024-07-10 13:55:33 +0530

    edit user operation implemented

--- crud_project/php-crud-app-main/edit-user.php
+++ crud_project/php-crud-app-main/edit-user.php
@@ -1,7 +1,30 @@
 <?php 
     include('./config/config.php');
+    // Getting users data by users id
+    if(isset($_GET['id'])){
+        $sql = "SELECT * FROM student WHERE id= " . $_GET['id'] ;
+        $result = $conn->query($sql);
+        $user = mysqli_fetch_assoc($result);
+        // var_dump($user);
+        // var_dump($user);
+    }else{
+        echo "Invalid Request!";
+        exit;
+    }
 
 
+    // update the user data
+    if(isset($_POST['submit'])){
+        extract($_POST); // use to extrat all POST array data
+        // query to update user details into database
+        $sql = "UPDATE student SET student_name = '$username' WHERE id = " . $_GET['id'];
+        if($conn->query($sql)){
+            echo "User Data Updated sucessfully!";
+        }
+        else{
+            echo "Something went wrong, data updation can not be proceed!";
+        }
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -18,12 +41,14 @@
     <section class="section">
         <h2>Edit User</h2>
 
-        <form action="" method="post">
+        <form action="edit-user.php?id=<?php echo $user['id'] ?>" method="post">
+
+        <!-- update the data with the dynamic values -->
             <div class="container">
                 <label for="uname"><b>Username</b></label>
-                <input type="text" placeholder="Enter Username" name="username" required value="">
+                <input type="text" placeholder="Enter Username" name="username" required value="<?php echo $user['student_name'] ?>">
                 <label for="psw"><b>Password</b></label>
-                <input type="text" placeholder="Enter Password" name="password" required value="">
+                <input type="password" placeholder="Enter Password" name="password" required value="<?php echo $user['password'] ?>">
                  
 
                 <button type="submit" name="submit">Update</button>
